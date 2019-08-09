74d373e4ec0976538a898c322b97828e7878fa96
austinbeaufort <abeaufort@sescollc.com>
2019-08-09 16:57:26 -0400

    added influx as database and using redis as cache for quick data retrieval

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const Influx = require('influx');
 const redis = require('redis');
+
+const bodyParser = require('body-parser');
 const util = require('util');
+
 let app = express();
 
 
@@ -11,6 +14,15 @@ client.on('error', error => console.log(`Error: ${error}`));
 
 let exists = util.promisify(client.exists.bind(client));
 let hgetall = util.promisify(client.hgetall.bind(client));
+let hmset = util.promisify(client.hmset.bind(client));
+
+// CONNECT INFLUX
+const influx = new Influx.InfluxDB({
+    host: 'localhost',
+    database: 'people',
+});
+
+
 
 // MIDDLEWARE FUNCTIONS
 
@@ -45,28 +57,57 @@ function createUserObject(obj, name) {
         name,
         lastname,
         age,
-        weight
+        weight,
     }
 
     return userObj;
 }
 
-async function gatherFunctions(body) {
+async function createPersonObject(userObj) {
+    let { age, lastname, name, user, weight } = userObj;
+    let personObj = {
+        name,
+        lastname,
+        age,
+        weight,
+        user
+    }
+    await hmset(name, 'lastname', lastname, 'age', age, 'weight', weight, 'user', user);
+    return personObj;
+}
+
+async function getInfluxData(name) {
+    return influx.query(`
+        select * from "userinfo"
+        where "name" = '${name}'
+    `);
+}
+
+
+
+async function gatherFunctions(body, response) {
     let name = body.name;
     let existsInRedis = await checkRedisForName(name);
-    console.log(existsInRedis);
+
     if(existsInRedis) {
         let redisObj = await getRedisData(name);
-        let userObj = createUserObject(redisObj, name);
-        return userObj;
+        let personObj = createUserObject(redisObj, name);
+        return personObj;
     } else {
-        console.log('no name found');
+        let influxObj = await getInfluxData(name);
+        let userObj = influxObj[0];
+        if(userObj === undefined) {
+            response.send('Requested Person not found...');
+        } else {
+            let personObj = await createPersonObject(userObj);
+            return personObj;
+        }
     }
 }
 
 
 function main(request, response) {
-    return gatherFunctions(request.body)
+    return gatherFunctions(request.body, response)
                 .then(reply => response.send(reply));
 }
 
